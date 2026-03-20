3d476497a92a9d533cf85558d877cc4d683ff410
taliecode <taliesinhillyert20@gmail.com>
2026-03-20 15:56:48 +0000

    added a loop so that the program stays active, given a (y/n) option to the user. added a .gitignore file

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,19 +1,30 @@
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
-print(f"sum: {num1 + num2}")
-
 operator = input("Enter operator (+ - * /): ")
-if operator == "+":
-    print(f"{num1} + {num2} = {num1 + num2}")
-elif operator == "-":
-    print(f"{num1} - {num2} = {num1 - num2}")
-elif operator == "*":
-    print(f"{num1} * {num2} = {num1 * num2}")
-elif operator == "/":
-    if num2 != 0:
-        print(f"{num1} / {num2} = {num1 / num2}")
+
+while True:
+    if operator == "+":
+        print(f"{num1} + {num2} = {num1 + num2}")
+    elif operator == "-":
+        print(f"{num1} - {num2} = {num1 - num2}")
+    elif operator == "*":
+        print(f"{num1} * {num2} = {num1 * num2}")
+    elif operator == "/":
+        if num2 != 0:
+            print(f"{num1} / {num2} = {num1 / num2}")
+        else:
+            print("Divide by zero!")
     else:
-        print("Divide by zero!")
-else:
-    print("Invalid")
+        print("Invalid")
+    
+    again = input("Next calculation? (y/n): ")
+    if again != "y":
+        break
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    operator = input("Enter operator (+ - * /): ")
+
+print("Goodbye!")
