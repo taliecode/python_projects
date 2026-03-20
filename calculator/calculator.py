b8cad7fa0ee0c069e97c6d689e388b3a914aef11
taliecode <taliesinhillyert20@gmail.com>
2026-03-20 15:37:51 +0000

    fixed indentation, Fixed operator conditional statements.

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -5,16 +5,15 @@ print(f"sum: {num1 + num2}")
 
 operator = input("Enter operator (+ - * /): ")
 if operator == "+":
-    print(f"{num1} + {num2} = {num1} + {num2}")
+    print(f"{num1} + {num2} = {num1 + num2}")
 elif operator == "-":
-    print(f"{num1} - {num2} = {num1} - {num2}")
+    print(f"{num1} - {num2} = {num1 - num2}")
 elif operator == "*":
-    print(f"{num1} * {num2} = {num1} * {num2}")
+    print(f"{num1} * {num2} = {num1 * num2}")
 elif operator == "/":
     if num2 != 0:
-        print(f"{num1} / {num2} = {num1} / {num2}")
+        print(f"{num1} / {num2} = {num1 / num2}")
     else:
         print("Divide by zero!")
 else:
-    print("Invalid operator!")
-    
+    print("Invalid")
