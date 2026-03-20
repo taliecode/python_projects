754c3786aacd2aad93ae14652bb8d772ebd3debe
taliecode <taliesinhillyert20@gmail.com>
2026-03-20 15:15:12 +0000

    Made a start by adding 2 float inputs, and operators

---
+++ calculator/calculator.py
@@ -0,0 +1,20 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print(f"sum: {num1 + num2}")
+
+operator = input("Enter operator (+ - * /): ")
+if operator == "+":
+    print(f"{num1} + {num2} = {num1} + {num2}")
+elif operator == "-":
+    print(f"{num1} - {num2} = {num1} - {num2}")
+elif operator == "*":
+    print(f"{num1} * {num2} = {num1} * {num2}")
+elif operator == "/":
+    if num2 != 0:
+        print(f"{num1} / {num2} = {num1} / {num2}")
+    else:
+        print("Divide by zero!")
+else:
+    print("Invalid operator!")
+    
